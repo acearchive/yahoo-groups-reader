76024c4fbcce729268217f52dffb49cc2f644847
Frawley <frawleyskid@gmail.com>
2022-04-20 18:40:49 -0400

    Fix multipart decoding not being triggered

--- parse/mime.go
+++ parse/mime.go
@@ -22,7 +22,7 @@ const (
 func MultipartMessageBody(email *mail.Message) (io.Reader, error) {
 	mediaType, contentTypeParams, err := mime.ParseMediaType(email.Header.Get(MimeHeaderContentType))
 
-	if err != nil && strings.HasPrefix(mediaType, contentTypePrefixMultipart) {
+	if err == nil && strings.HasPrefix(mediaType, contentTypePrefixMultipart) {
 		multipartReader := multipart.NewReader(email.Body, contentTypeParams[contentTypeParamBoundary])
 		for {
 			part, err := multipartReader.NextPart()
