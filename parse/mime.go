9958ba996e0cb196d69c819a1eee3d16ae44ae43
Frawley <frawleyskid@gmail.com>
2022-04-21 09:48:56 -0400

    Decode charset in multipart messages

--- parse/mime.go
+++ parse/mime.go
@@ -21,7 +21,20 @@ const (
 	quotedPrintable                   = "quoted-printable"
 )
 
-func decodeMultipart(email *mail.Message) (io.Reader, error) {
+func decodeCharset(body io.Reader, contentTypeParams map[string]string) io.Reader {
+	if charset, hasCharset := contentTypeParams[contentTypeParamCharset]; hasCharset {
+		encoding, err := ianaindex.MIME.Encoding(charset)
+		if err != nil {
+			return body
+		}
+
+		return encoding.NewDecoder().Reader(body)
+	}
+
+	return body
+}
+
+func DecodeMessageBody(email *mail.Message) (io.Reader, error) {
 	mediaType, contentTypeParams, err := mime.ParseMediaType(email.Header.Get(MimeHeaderContentType))
 
 	if err == nil && strings.HasPrefix(mediaType, contentTypePrefixMultipart) {
@@ -34,44 +47,20 @@ func decodeMultipart(email *mail.Message) (io.Reader, error) {
 				return nil, err
 			}
 
-			mediaType, _, err := mime.ParseMediaType(part.Header.Get(MimeHeaderContentType))
+			partMediaType, partContentTypeParams, err := mime.ParseMediaType(part.Header.Get(MimeHeaderContentType))
 			if err != nil {
 				return nil, err
 			}
 
-			if mediaType == contentTypePlainText {
-				return part, nil
+			if partMediaType == contentTypePlainText {
+				return decodeCharset(part, partContentTypeParams), nil
 			}
 		}
 	}
 
 	if email.Header.Get(MimeHeaderContentTransferEncoding) == quotedPrintable {
-		return quotedprintable.NewReader(email.Body), nil
+		return decodeCharset(quotedprintable.NewReader(email.Body), contentTypeParams), nil
 	}
 
-	return email.Body, nil
-}
-
-func decodeCharset(body io.Reader, contentType string) (io.Reader, error) {
-	_, contentTypeParams, err := mime.ParseMediaType(contentType)
-
-	if charset, hasCharset := contentTypeParams[contentTypeParamCharset]; hasCharset && err == nil {
-		encoding, err := ianaindex.MIME.Encoding(charset)
-		if err != nil {
-			return body, nil
-		}
-
-		return encoding.NewDecoder().Reader(body), nil
-	}
-
-	return body, nil
-}
-
-func DecodeMessageBody(email *mail.Message) (io.Reader, error) {
-	multipartDecoded, err := decodeMultipart(email)
-	if err != nil {
-		return nil, err
-	}
-
-	return decodeCharset(multipartDecoded, email.Header.Get(MimeHeaderContentType))
+	return decodeCharset(email.Body, contentTypeParams), nil
 }
