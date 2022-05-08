818160942685f0f60fe7c9de0fae3abce9021131
Frawley <frawleyskid@gmail.com>
2022-05-08 18:55:39 -0400

    Use instant.page to preload pages

--- pipeline/gulpfile.js
+++ pipeline/gulpfile.js
@@ -5,6 +5,9 @@ const htmlmin = require("gulp-htmlmin");
 const del = require("delete");
 const createIndex = require("./search.js");
 const webpack = require("webpack-stream");
+const concat = require("gulp-concat");
+const named = require("vinyl-named");
+const whitelister = require("purgecss-whitelister");
 const path = require("path");
 
 const { series, parallel, src, dest } = require("gulp");
@@ -15,50 +18,6 @@ const publicDir = process.env.PUBLIC_DIR ?? "../public"
 const jsDest = path.join(publicDir, "js")
 const cssDest = path.join(publicDir, "css")
 const fontDest = path.join(publicDir, "font")
-
-function bootstrapCss() {
-    return src("node_modules/bootstrap/dist/css/bootstrap.css")
-        .pipe(purgeCss({
-            content: [
-                path.join(outputDir, "**/*.html"),
-                "./node_modules/bootstrap/js/src/collapse.js",
-            ],
-        }))
-        .pipe(cleanCss())
-        .pipe(rename({ extname: ".min.css" }))
-        .pipe(dest(cssDest));
-}
-
-function css() {
-    return src("src/*.css")
-        .pipe(cleanCss())
-        .pipe(rename({ extname: ".min.css" }))
-        .pipe(dest(cssDest));
-}
-
-function bootstrapJs() {
-    return src("src/bootstrap.js")
-        .pipe(webpack({
-            mode: "production",
-            devtool: "source-map",
-            output: {
-                filename: "bootstrap.min.js"
-            },
-        }))
-        .pipe(dest(jsDest));
-}
-
-function searchJs() {
-    return src("src/search.js")
-        .pipe(webpack({
-            mode: "production",
-            devtool: "source-map",
-            output: {
-                filename: "search.min.js"
-            }
-        }))
-        .pipe(dest(jsDest))
-}
 
 function html() {
     const options = {
@@ -76,6 +35,42 @@ function html() {
         .pipe(dest(publicDir));
 }
 
+function css() {
+    return src([
+        "node_modules/bootstrap/dist/css/bootstrap.css",
+        "src/variables.css",
+        "src/*.css",
+    ])
+        .pipe(concat("bundle.css"))
+        .pipe(purgeCss({
+            content: [
+                path.join(outputDir, "**/*.html"),
+                "node_modules/bootstrap/js/src/collapse.js",
+            ],
+            safelist: [
+                ...whitelister([
+                    "./src/search.css",
+                ])
+            ],
+        }))
+        .pipe(cleanCss())
+        .pipe(rename({ extname: ".min.css" }))
+        .pipe(dest(cssDest));
+}
+
+function js() {
+    return src("src/*.js")
+        .pipe(named())
+        .pipe(webpack({
+            mode: "production",
+            devtool: "source-map",
+            output: {
+                filename: "[name].min.js"
+            },
+        }))
+        .pipe(dest(jsDest));
+}
+
 function headers() {
     return src("src/index.headers")
         .pipe(rename("_headers"))
@@ -87,14 +82,14 @@ const fontWeights = ["300", "400", "500"];
 function fontCss() {
     return src(
         fontWeights.map(weight => `node_modules/@fontsource/noto-sans/${weight}.css`)
-    ).pipe(dest(`${fontDest}/noto-sans`));
+    ).pipe(dest(path.join(fontDest, "noto-sans")));
 }
 
 function fontFiles() {
     return src([
         ...fontWeights.map(weight => `node_modules/@fontsource/noto-sans/files/noto-sans-*-${weight}-normal.woff`),
         ...fontWeights.map(weight => `node_modules/@fontsource/noto-sans/files/noto-sans-*-${weight}-normal.woff2`),
-    ]).pipe(dest(`${fontDest}/noto-sans/files`));
+    ]).pipe(dest(path.join(fontDest, "noto-sans", "files")));
 }
 
 function cleanOutput() {
@@ -111,12 +106,10 @@ function buildSearchIndex() {
 
 const font = parallel(fontCss, fontFiles);
 
-const bootstrap = parallel(bootstrapCss, bootstrapJs);
-
 exports.clean = parallel(cleanOutput, cleanPublic);
 
 exports.default = series(
     cleanPublic,
-    parallel(bootstrap, searchJs, css, html, font, headers, buildSearchIndex),
+    parallel(html, css, js, font, headers, buildSearchIndex),
     cleanOutput,
 );
