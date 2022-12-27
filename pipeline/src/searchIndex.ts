9c76af088e26dad1aa9c5a860f974e1e0eabc80f
Frawley <frawley@duck.com>
2022-12-27 11:53:48 -0500

    Add cache-buster URL for search index

--- pipeline/src/searchIndex.ts
+++ pipeline/src/searchIndex.ts
@@ -1,9 +1,11 @@
 import fs from "fs/promises";
 import path from "path";
 import FlexSearch from "flexsearch";
+import hasha from "hasha";
 
 const searchFileName = "search.json";
-const indexDirName = "search";
+
+const indexDirName = (hash: string) => `search/index/${hash}`;
 
 export interface MessageFields {
   id: number;
@@ -18,11 +20,34 @@ export interface MessageFields {
 
 export type SearchIndex = FlexSearch.Document<MessageFields, string[]>;
 
+type SearchSettings = FlexSearch.IndexOptionsForDocumentSearch<
+  MessageFields,
+  string[]
+>;
+
+const flexsearchSettings: SearchSettings = {
+  preset: "memory",
+  document: {
+    id: "id",
+    store: ["id", "page", "timestamp", "user", "title", "body"],
+    index: ["user", "flair", "year", "title", "body"],
+  },
+};
+
+// An object we can hash to generate a cache-buster URL for the search index.
+type SearchIndexHashObject = {
+  settings: SearchSettings;
+  data: ReadonlyArray<MessageFields>;
+};
+
+const hashSearchIndex = (obj: SearchIndexHashObject): Promise<string> =>
+  hasha.async(JSON.stringify(obj), { encoding: "hex", algorithm: "sha256" });
+
 export const createSearchIndex = async (
   inputDir: string,
   outputDir: string
 ) => {
-  let searchData;
+  let searchData: ReadonlyArray<MessageFields>;
 
   try {
     searchData = JSON.parse(
@@ -32,20 +57,18 @@ export const createSearchIndex = async (
     return;
   }
 
-  const index: SearchIndex = new FlexSearch.Document({
-    preset: "memory",
-    document: {
-      id: "id",
-      store: ["id", "page", "timestamp", "user", "title", "body"],
-      index: ["user", "flair", "year", "title", "body"],
-    },
-  });
+  const index: SearchIndex = new FlexSearch.Document(flexsearchSettings);
 
   await Promise.all(
     searchData.map((fields: MessageFields) => index.addAsync(fields.id, fields))
   );
 
-  const indexDir = path.join(outputDir, indexDirName);
+  const searchIndexHash = await hashSearchIndex({
+    settings: flexsearchSettings,
+    data: searchData,
+  });
+
+  const indexDir = path.join(outputDir, indexDirName(searchIndexHash));
   await fs.mkdir(indexDir, { recursive: true });
 
   await index.export(async (key, data) => {
